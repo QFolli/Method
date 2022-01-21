1f232d9899b9b19efb76e50c15f6b1f56b0231f5
Виталий Моисеенко <88559269+QFolli@users.noreply.github.com>
2022-01-21 11:32:31 +0200

    Add files via upload

---
+++ PatternMethod/PatternMethod/Coffee.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatternMethod
+{
+    public class Coffee : CaffeineBeverage
+    {
+        private bool userAnswer;
+        public Coffee(bool answer)
+        {
+            userAnswer = answer;
+        }
+        protected override void Brew()
+        {
+            Console.WriteLine("Завариваем кофе");
+        }
+
+        protected override void AddCondiments()
+        {
+            Console.WriteLine("Добавляем молоко");
+        }
+
+        protected override bool CustomerWantsCondiments()
+        {
+            return userAnswer;
+        }
+    }
+}
---
+++ PatternMethod/PatternMethod/Tea.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace PatternMethod
+{
+    public class Tea : CaffeineBeverage
+    {
+        protected override void Brew()
+        {
+            Console.WriteLine("Завариваем чай");
+        }
+        protected override void AddCondiments()
+        {
+            Console.WriteLine("Добавляем сахар");
+        }
+    }
+}
